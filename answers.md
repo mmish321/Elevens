d83d0d7a70aac178bf0171a406a5ea6158a58cf0
Michelle Mao <maom@s.dcsdk12.org>
2016-03-11 10:05:30 -0700

    Finished exercise 1 and 2 , finished the section

--- answers.md
+++ answers.md
@@ -1,4 +1,5 @@
 # Activity 1: Design and Create a Card Class 
 
 ## Exercises 
-1. 
+1. Finished all the getter methods and constructor  
+2. Tested all the methods created with success
--- src/CardTester.java
+++ src/CardTester.java
@@ -8,6 +8,13 @@ public class CardTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card queenOfSpades = new Card("Queen", "Spades", 5);
+		Card kingOfHearts = new Card("King", "Hearts", 6);
+		Card tenOfClover = new Card("ten", "Clover", 3);
+		System.out.println(queenOfSpades.suit());
+		System.out.println(queenOfSpades.toString());
+		System.out.println(queenOfSpades.pointValue());
+		System.out.println(kingOfHearts.toString());
+		
 	}
 }
